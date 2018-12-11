7ba68176e7f911b4b6458df3bf090b992d1785f7
killi8n <killi8n@killi8nui-MacBookPro.local>
2018-12-11 10:05:10 +0900

    removed service worker plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -44,19 +44,19 @@ module.exports = {
         pathToConfigModule: `src/utils/typography`
       }
     },
-    {
-      resolve: `gatsby-plugin-manifest`,
-      options: {
-        name: "GatsbyJS",
-        short_name: "GatsbyJS",
-        start_url: "/",
-        background_color: "#6b37bf",
-        theme_color: "#6b37bf",
-        display: "minimal-ui",
-        icon: "src/images/icon.png" // This path is relative to the root of the site.
-      }
-    },
-    "gatsby-plugin-offline",
+    // {
+    //   resolve: `gatsby-plugin-manifest`,
+    //   options: {
+    //     name: "GatsbyJS",
+    //     short_name: "GatsbyJS",
+    //     start_url: "/",
+    //     background_color: "#6b37bf",
+    //     theme_color: "#6b37bf",
+    //     display: "minimal-ui",
+    //     icon: "src/images/icon.png" // This path is relative to the root of the site.
+    //   }
+    // },
+    // "gatsby-plugin-offline",
     `gatsby-plugin-react-helmet`,
     `gatsby-plugin-react-helmet`
   ]
